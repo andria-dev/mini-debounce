5cfee6f2953d64d70f51071ae7b696dee51d0f71
Christopher Brown <christopherbrown@Christophers-Mac-mini.local>
2019-03-09 23:28:36 -0700

    create files

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.debounce = (callback, timeout) => {
+  let id;
+  return (...args) => {
+    clearTimeout(id);
+    return id = setTimeout(() => callback(...args), timeout);
+  };
+};
---
+++ index.mjs
@@ -0,0 +1,7 @@
+export let debounce = (callback, timeout) => {
+  let id;
+  return (...args) => {
+    clearTimeout(id);
+    return id = setTimeout(() => callback(...args), timeout);
+  };
+};
